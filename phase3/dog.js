e21952ee0f6bbcc4438393a94136bd8edd367405
caitlynvaliente <caitlyn_valiente@davincischools.org>
2017-12-05 14:42:05 -0800

    Add files via upload

---
+++ phase3/dog.js
@@ -0,0 +1,9 @@
+function showText(e){
+	document.getElementById("text").classList.toggle("disappear");
+}
+function other(elem){
+	elem.classList.toggle("new1");
+}
+function other2(elem){
+	elem.classList.toggle("new2");
+}
---
+++ phase3/hoves.js
@@ -0,0 +1,6 @@
+function other3(elem){
+	elem.classList.toggle("new3");
+}
+function other4(elem){
+	elem.classList.toggle("new4");
+}
