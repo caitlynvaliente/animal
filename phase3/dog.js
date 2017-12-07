0b00940e0e4cc85a3e5a67728cd482035d395b1f
caitlynvaliente <caitlyn_valiente@davincischools.org>
2017-12-07 14:31:49 -0800

    Update dog.js

--- phase3/dog.js
+++ phase3/dog.js
@@ -7,3 +7,23 @@ function other(elem){
 function other2(elem){
 	elem.classList.toggle("new2");
 }
+function other8(elem){
+	elem.classList.toggle("gif");
+}
+function other9(elem){
+	elem.classList.toggle("whitepage");
+}
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 9000);    
+}
