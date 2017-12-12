e1beaa1413d896407f9f9c7f546d092f32578a35
caitlynvaliente <caitlyn_valiente@davincischools.org>
2017-12-11 20:10:53 -0800

    Update hoves.js

--- final/hoves.js
+++ final/hoves.js
@@ -23,5 +23,5 @@ function carousel() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 9000);    
+    setTimeout(carousel, 5000);    
 }
